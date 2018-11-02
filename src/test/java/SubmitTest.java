b2ac1f46a2c937db1db519369a418646a5d5f82c
Roman <klimov_ru@rambler.ru>
2018-11-02 07:56:50 +0300

    + oop

---
+++ src/test/java/SubmitTest.java
@@ -0,0 +1,12 @@
+
+public class LoginTest  extends TestBase{
+    @Override
+    public void testLogin() {
+        super.testLogin();
+    }
+
+    @Override
+    public void testSubmit() {
+        super.testSubmit();
+    }
+}
